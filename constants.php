eecf97a9b61274c76db599268efabd470369edfb
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-02-15 00:46:23 -0600

    initial commit

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+
+const FULL_NAME = "Anthony Cowden";
+const PHONE = "618-207-5399";
+const EMAIL = "acowden@yahoo.com";
+const LOCATION = "Joliet, IL";
+const LINKED_IN = "https://www.linkedin.com/in/anthony-cowden-b3854221/";
+
+?>
