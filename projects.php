c0ca9152945c02aa6d62092df66a99f86b4cbdc7
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-02-15 00:45:44 -0600

    initial commit

---
+++ projects.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'htmlComponents/head.php'; ?>
+  <body>
+    <?php include 'htmlComponents/header.php'; ?>
+
+    <div class="container-fluid d-flex justify-content-center project-container">
+
+         <div class="row">
+            <div class="col">
+                <h2>Charity Shop</h2>
+               
+            </div>
+        </div>  
+      
+    </div>
+
+
+  </body>
+</html>
