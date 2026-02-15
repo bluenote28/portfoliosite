899524faa07caecc0f13fffa57ba755786cc64fc
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-02-15 00:48:15 -0600

    adds contact bar and begins professional experience section

--- resume.php
+++ resume.php
@@ -1,8 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'htmlComponents/head.php'; ?>
+<?php include 'htmlComponents/head.php'; include_once 'constants.php' ?>
   <body>
     <?php include 'htmlComponents/header.php'; ?>
+
+    <h1 style="text-align:center" class="mt-5"><?php echo FULL_NAME  ?></h1>
+
+    <ul class='nav justify-content-center'>
+        <li class="nav-item mx-1">
+            <?php echo PHONE ?>
+        </li>
+        <li class="nav-item mx-1">
+          |
+        </li>
+        <li class="nav-item mx-1">
+            <?php echo EMAIL ?>
+        </li>
+        <li class="nav-item mx-1">
+          |
+        </li>
+         <li class="nav-item mx-1">
+            <?php echo LOCATION ?>
+        </li>
+        <li class="nav-item mx-1">
+          |
+        </li>
+         <li class="nav-item mx-1">
+            <a href='<?php echo LINKED_IN ?>'>LinkedIn</a>
+        </li>
+    </ul>
+
+    <div class="container mt-5">
+      <h4>PROFESSIONAL EXPERIENCE</h4>
+      <hr />
+    </div>
+
+
 
   </body>
 </html>
