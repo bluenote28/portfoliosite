73a9613d595aea53e1638a7d31463a508422155c
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-02-15 00:48:47 -0600

    adds welcome message

--- index.php
+++ index.php
@@ -4,5 +4,9 @@
   <body>
     <?php include 'htmlComponents/header.php'; ?>
 
+    <div class="container-fluid d-flex justify-content-center align-items-top" style='background-color: #243444; height: 50px;'>
+      <p style='color: #ffffff'>Thank you for visiting my site and considering me for a position with your organization.</p>
+    </div>
+
   </body>
 </html>
