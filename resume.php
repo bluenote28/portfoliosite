574acf5a5ca3669e1029ac58f08a3321f751f584
Anthony Cowden <ancowden@gmail.com>
2026-02-16 09:08:49 -0600

    adds education and skills

--- resume.php
+++ resume.php
@@ -33,9 +33,40 @@
     <div class="container mt-5">
       <h4>PROFESSIONAL EXPERIENCE</h4>
       <hr />
+
+      <div class="mb-5">
+        <div>
+          <h5>Amazon Application Development Special Interest Group Member</h5>
+          <p class="mb-0">Amazon | Monee, IL | January 2023 - Current</p>
+        </div>
+        <ul>
+          <li>I develop and support internal tools to be used by Amazon's IT organization.</li>
+          <li>I built an Accomplishment Tracker application using React and Python. The site is hostes on AWS.</li>
+        </ul>
+
+      </div>
+
+      <h4>EDUCATION</h4>
+      <hr />
+      <div class="mb-5">
+        <div class="mb-3">
+          <h5>Western Governors University</h5>
+          <p class="mb-0">Bachelor of Science in Software Engineering | Salt Lake City, UT | March 2023 - November 2025</p>
+        </div>
+         <div>
+          <h5>Southwestern Illinois College</h5>
+          <p class="mb-0">Associate of Science in Computer Information Systems | Belleville, IL | June 2011 - December 2012</p>
+        </div>
     </div>
 
-
+     <h4>Skills</h4>
+     <hr />
+      <ul>
+        <li>Programming Languages: Python, JavaScript, PHP, SQL</li>
+        <li>Frameworks and Libraries: React, Flask, Django</li>
+        <li>Tools and Platforms: AWS, Git, Docker</li>
+        <li>Soft Skills: Problem-solving, Communication, Teamwork</li>
+      </ul>
 
   </body>
 </html>
