e272e525e893301c9fd4e8ee6466a0b1100da119
Anthony Cowden <ancowden@gmail.com>
2026-02-16 21:23:09 -0600

    rewritten to use projects objects

--- projects.php
+++ projects.php
@@ -2,44 +2,11 @@
 <html lang="en">
 <?php include 'htmlComponents/head.php'; ?>
   <body>
-    <?php include 'htmlComponents/header.php'; ?>
-
-    <div class="project-container"> 
-      <h1>Charity Shop</h1>
-      <p>This is a wrapper over Ebay to help you locate items that benefit charity. This was built to solve my own personal frustrations at using Ebay's site to locate charity items.</p>
-      <p>Skills Used: HTML, CSS, React, Python, Django, Postgresql</p>
-      <div><a class="btn btn-primary" href="https://charityshopusa.com" role="button">Link</a>
-           <a class="btn btn-secondary" href="https://github.com/bluenote28/CharityShopFrontEnd" role="button">GitHub Repo</a></div>
-      <img src="images/charityshop.png" alt="Charity Shop Project Image" class="project-image">
-    </div> 
-
-     <div class="project-container"> 
-      <h1>Bible Catholic</h1>
-      <p>This was created to create a site where biblical data for the catholic church can be easily found.</p>
-      <p>Skills Used: HTML, CSS, Python, Flask</p>
-      <div><a class="btn btn-primary" href="https://www.biblecatholic.net/" role="button">Link</a>
-           <a class="btn btn-secondary" href="https://github.com/bluenote28/biblecatholic" role="button">GitHub Repo</a></div>
-      <img src="images/biblecatholic.png" alt="Bible Catholic Project Image" class="project-image">
-    </div> 
-
-    <div class="project-container"> 
-      <h1>Accomplishment Tracker</h1>
-      <p>This is a site where Amazon employees can track their work accomplishments and easily share them with their manager.
-        This was made in response to an issue I experienced at Amazon where I had three different managers within a short time periord. Each manager had no idea of my past work. 
-        This site solved that problem. Since this was made for internal use at Amazon, I cannot share a link to the site or the code.
-        I do have permission to privately share pictures of the site in an interview.
-      </p>
-      <p>Skills Used: React, Python, AWS</p>
-    </div> 
-
-    <div class="project-container"> 
-      <h1>Toolshed</h1>
-      <p>At Amazon, the tools that we use in the IT deparment are distributed across many different sites, which makes it difficult to find the tools you need to do your job. 
-        Toolshed is a site that I created to solve this problem by aggregating all the tools in one place. 
-        Since this was made for internal use at Amazon, I cannot share a link to the site or the code. I do have permission to privately share pictures of the site in an interview.
-      </p>
-      <p>Skills Used: React, AWS</p>
-    </div>
-
+    <?php include 'htmlComponents/header.php'; include_once 'constants.php'; 
+    
+    foreach (PROJECTS as $project) { 
+        $project->display();
+    }
+    ?>
   </body>
 </html>
